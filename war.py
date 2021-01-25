254a46dcfd748c7d897cb127a024755c1452397b
Rooooopert <64103215+Umin24@users.noreply.github.com>
2021-01-25 23:05:32 +0300

    War update

--- war.py
+++ war.py
@@ -41,12 +41,13 @@ WIDTH, HEIGHT = infoObject.current_w, infoObject.current_h
 MAPS = [load_map('map1')[0], load_map('map2')[0],
         load_map('map3')[0], load_map('map4')[0], load_map('map5')[0]]
 
-FPS = 200
+FPS = 400
 STEP = 50
 
-PRIC = {29: 50, 32: 50, 35: 50,
-        30: 100, 33: 100, 36: 100,
-        31: 200, 34: 200, 37: 200}
+PRIC = {29: 50, 32: 50, 35: 50, 38: 50,
+        30: 100, 33: 100, 36: 100, 39: 100,
+        31: 200, 34: 200, 37: 200, 40: 200,
+        27: 100, 28: 100}
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
@@ -60,8 +61,28 @@ boardmenu_sprites = pygame.sprite.Group()
 tiles_choose = pygame.sprite.Group()
 
 tile_list = []
+knight_list = []
 
 choosing = '023'
+
+
+class Sprite:
+    def __init__(self, name, size, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.size = size
+
+        self.sprite = pygame.sprite.Sprite()
+        image = pygame.image.load(f'{name}.png')
+        image = pygame.transform.scale(image, size)
+        self.sprite.image = image
+        self.sprite.rect = self.sprite.image.get_rect()
+        self.sprite.rect.x = self.x
+        self.sprite.rect.y = self.y
+
+    def get_sprite(self):
+        return self.sprite
 
 
 class RedactorMenu:
@@ -82,9 +103,6 @@ class RedactorMenu:
         redactormenu_sprites.add(self.sprite5)
 
         self.txt5 = Text('CReATe', 677, 105, 55, (255, 255, 255))
-
-
-
 
 
         self.but6 = Button('Idle 1', 50, 180)
@@ -117,19 +135,12 @@ class RedactorMenu:
         self.txt10 = Text(MAPS[4], 110, 470, 55, (255, 255, 255))
 
 
-
-
-        textbox = pygame.sprite.Sprite()
-        image = load_image('textbox.png')
-        image = pygame.transform.scale(image, (565, 60))
-        textbox.image = image
-        textbox.rect = textbox.image.get_rect()
-        textbox.rect.x = 45
-        textbox.rect.y = 95
+        box = Sprite('data/textbox', (565, 60), 45, 95)
+        textbox = box.get_sprite()
         redactormenu_sprites.add(textbox)
 
     def render(self, screen):
-        fon = pygame.transform.scale(load_image('fon4.jpg'), (self.w, self.h))
+        fon = pygame.transform.scale(load_image('fon.jpg'), (self.w, self.h))
         screen.blit(fon, (0, 0))
 
         pygame.draw.rect(screen, pygame.Color('white'), self.input_box)
@@ -202,13 +213,19 @@ class Redactor:
     def change_tile(self, index):
         global choosing
 
+        if int(choosing) >= 100:
+            choosing = '023'
         self.map[index[0]][index[1]] = choosing
         self.draw_one(index[0], index[1])
 
 
 class TilesChoose:
-    def __init__(self):
+    def __init__(self, money, profit):
+        self.money = money
+        self.profit = profit
         self.draw()
+
+
 
     def render(self, screen):
         pass
@@ -280,6 +297,10 @@ class TilesChoose:
             tiles_choose.add(sprite)
             tile_list.append(sprite)
 
+        knight1 = Sprite('knight/1knight00', (128, 128), 800, 0).get_sprite()
+        tiles_choose.add(knight1)
+        knight_list.append(knight1)
+
         for i in range(3):
             r = str(i + 32).rjust(3, '0')
             sprite = pygame.sprite.Sprite()
@@ -290,6 +311,11 @@ class TilesChoose:
             sprite.rect.center = (32 + 96 * (i + 6), 240)
             tiles_choose.add(sprite)
             tile_list.append(sprite)
+
+        knight2 = Sprite('knight/2knight00', (128, 128), 800, 100).get_sprite()
+        tiles_choose.add(knight2)
+        knight_list.append(knight2)
+
 
         for i in range(3):
             r = str(i + 35).rjust(3, '0')
@@ -302,6 +328,10 @@ class TilesChoose:
             tiles_choose.add(sprite)
             tile_list.append(sprite)
 
+        knight3 = Sprite('knight/3knight00', (128, 128), 800, 200).get_sprite()
+        tiles_choose.add(knight3)
+        knight_list.append(knight3)
+
         for i in range(3):
             r = str(i + 38).rjust(3, '0')
             sprite = pygame.sprite.Sprite()
@@ -313,13 +343,41 @@ class TilesChoose:
             tiles_choose.add(sprite)
             tile_list.append(sprite)
 
+        knight4 = Sprite('knight/4knight00', (128, 128), 600, 300).get_sprite()
+        tiles_choose.add(knight4)
+        knight_list.append(knight4)
+
+
+        self.text = Text(f'MOney: {self.money}', 20, 600, 55, (0, 0, 0))
+        self.text2 = Text(f'profit: {self.profit}', 20, 700, 55, (0, 0, 0))
+
+
+class AnimatedSprite:
+    def __init__(self, name, size, x, y, key):
+        self.cur = 0
+        self.size = size
+        self.key = key
+        self.knight = Sprite(name, size, x, y).get_sprite()
+        all_sprites_game.add(self.knight)
+
+    def update(self, limit):
+        self.cur += 1
+        image = pygame.image.load(f"knight/{self.key}knight{str(self.cur).rjust(2, '0')}.png")
+        image = pygame.transform.scale(image, self.size)
+        self.knight.image = image
+
+        self.cur %= limit
+
+
 
 class Board:
     def __init__(self, width, height, x, y, map, key):
         self.w = width
         self.h = height
 
-        self.order = 1
+        self.knights = []
+        self.knightmap = []
+
 
         self.x = x
         self.y = y
@@ -327,12 +385,12 @@ class Board:
         self.key = key
         self.map = map
 
-        self.money1 = 100
-        self.money2 = 100
-        self.money3 = 100
-        self.money4 = 100
-
         self.maptxt()
+        self.put_money()
+        self.get_profit()
+        self.knightmaper()
+
+        self.limit = 4
 
         self.centers = [[] for _ in range(14)]
 
@@ -344,8 +402,52 @@ class Board:
             for i in range(14):
                 f.write('\n' + ''.join(self.map[i]))
 
-    def render(self, screen):
-        pass
+    def knightmaper(self):
+        with open(f'maps/knight{self.key}.txt', 'rt') as f:
+            map_lines = f.read().strip().split('\n')
+            for _ in range(len(map_lines)):
+                knight_line = [map_lines[_][i:i + 3] for i in range(0, len(map_lines[_]), 3)]
+                self.knightmap.append(knight_line)
+
+    def get_profit(self):
+        self.profits = []
+        a = 0
+        b = 0
+        c = 0
+        d = 0
+        for x in self.map:
+            for y in x:
+                if 29 <= int(y) <= 31:
+                    a += 10
+                elif 38 <= int(y) <= 40:
+                    b += 10
+                elif 32 <= int(y) <= 34:
+                    c += 10
+                elif 35 <= int(y) <= 37:
+                    d += 10
+
+        self.profits = [a, b, c, d]
+
+
+    def put_money(self):
+        with open('maps/money.txt', 'rt') as f:
+            self.money = f.readlines()[self.key - 1].strip().split()
+            self.money = [int(x) for x in self.money]
+            self.order = self.money.pop()
+
+    def save_game(self):
+        with open('maps/money.txt', 'rt') as f:
+            file = f.readlines()
+            money_data = self.money + [self.order]
+            money_data = [str(x) for x in money_data]
+            file[self.key - 1] = ' '.join(money_data) + '\n'
+
+        with open(f'maps/knight{self.key}.txt', 'wt') as f:
+            knightmapline = [''.join(x) for x in self.knightmap]
+            f.write('\n'.join(knightmapline))
+
+        with open('maps/money.txt', 'wt') as f:
+            f.write(''.join(file))
 
     def draw_one(self, x, y):
         sprite = pygame.sprite.Sprite()
@@ -384,33 +486,127 @@ class Board:
         self.sprite11 = self.but11.get_sprite()
         all_sprites_game.add(self.sprite11)
 
+        self.but12 = Button('skip', WIDTH - 60, HEIGHT - 60)
+        self.sprite12 = self.but12.get_sprite()
+        all_sprites_game.add(self.sprite12)
+
+        for i in range(len(self.knightmap)):
+            self.knights.append([])
+            for j in range(len(self.knightmap[i])):
+                if self.knightmap[i][j] != '000':
+                    knight = AnimatedSprite(f'knight/{int(self.knightmap[i][j])}knight00', (128, 128),
+                                            self.centers[i][j][0] - 56, self.centers[i][j][1] - 64, int(self.knightmap[i][j]))
+                    self.knights[i].append(knight)
+                else:
+                    self.knights[i].append(None)
+
     def change_tile(self, index):
         global choosing
 
-        if self.order == 1 and 1 <= int(choosing) <= 6 and self.money1 >= 10:
+        if self.knightmap[index[0]][index[1]] != '000':
+            return
+
+        if self.order == 1 and choosing == '100':
+            if self.money[self.order - 1] >= 20:
+                knight = AnimatedSprite('knight/1knight00', (128, 128), self.centers[index[0]][index[1]][0] - 56,
+                                        self.centers[index[0]][index[1]][1] - 64, 1)
+                self.money[self.order - 1] -= 20
+                self.knights[index[0]].append(knight)
+                self.knightmap[index[0]][index[1]] = choosing[::-1]
+
+        if self.order == 2 and choosing == '200':
+            if self.money[self.order - 1] >= 20:
+                knight = AnimatedSprite('knight/2knight00', (128, 128), self.centers[index[0]][index[1]][0] - 56,
+                                        self.centers[index[0]][index[1]][1] - 64, 2)
+                self.money[self.order - 1] -= 20
+                self.knights[index[0]].append(knight)
+                self.knightmap[index[0]][index[1]] = choosing[::-1]
+
+        if self.order == 3 and choosing == '300':
+            if self.money[self.order - 1] >= 20:
+                knight = AnimatedSprite('knight/3knight00', (128, 128), self.centers[index[0]][index[1]][0] - 56,
+                                        self.centers[index[0]][index[1]][1] - 64, 3)
+                self.money[self.order - 1] -= 20
+                self.knights[index[0]].append(knight)
+                self.knightmap[index[0]][index[1]] = choosing[::-1]
+
+        if self.order == 4 and choosing == '400':
+            if self.money[self.order - 1] >= 20:
+                knight = AnimatedSprite('knight/4knight00', (128, 128), self.centers[index[0]][index[1]][0] - 56,
+                                        self.centers[index[0]][index[1]][1] - 64, 4)
+                self.money[self.order - 1] -= 20
+                self.knights[index[0]].append(knight)
+                self.knightmap[index[0]][index[1]] = choosing[::-1]
+
+
+        if self.order == 1 and 1 <= int(choosing) <= 6 and self.money[self.order - 1] >= 10:
             self.map[index[0]][index[1]] = choosing
             self.draw_one(index[0], index[1])
-            self.money1 -= 10
+            self.money[self.order - 1] -= 10
 
         if self.order == 1 and 29 <= int(choosing) <= 31:
-            if self.money1 >= PRIC[int(choosing)]:
+            if self.money[self.order - 1] >= PRIC[int(choosing)]:
                 self.map[index[0]][index[1]] = choosing
                 self.draw_one(index[0], index[1])
-                self.money1 -= PRIC[int(choosing)]
-        
-        if self.order == 2 and 7 <= int(choosing) <= 12 and self.money1 >= 10:
+                self.money[self.order - 1] -= PRIC[int(choosing)]
+
+
+        if self.order == 2 and 7 <= int(choosing) <= 12 and self.money[self.order - 1] >= 10:
             self.map[index[0]][index[1]] = choosing
             self.draw_one(index[0], index[1])
-            self.money2 -= 10
+            self.money[self.order - 1] -= 10
 
-        if self.order == 2 and 32 <= int(choosing) <= 34:
-            if self.money1 >= PRIC[int(choosing)]:
+        if self.order == 2 and 38 <= int(choosing) <= 40:
+            if self.money[self.order - 1] >= PRIC[int(choosing)]:
                 self.map[index[0]][index[1]] = choosing
                 self.draw_one(index[0], index[1])
-                self.money2 -= PRIC[int(choosing)]
+                self.money[self.order - 1] -= PRIC[int(choosing)]
+
+
+        if self.order == 3 and 13 <= int(choosing) <= 18 and self.money[self.order - 1] >= 10:
+            self.map[index[0]][index[1]] = choosing
+            self.draw_one(index[0], index[1])
+            self.money[self.order - 1] -= 10
+
+        if self.order == 3 and 32 <= int(choosing) <= 34:
+            if self.money[self.order - 1] >= PRIC[int(choosing)]:
+                self.map[index[0]][index[1]] = choosing
+                self.draw_one(index[0], index[1])
+                self.money[self.order - 1] -= PRIC[int(choosing)]
+
+
+        if self.order == 4 and 19 <= int(choosing) <= 22 and self.money[self.order - 1] >= 10:
+            self.map[index[0]][index[1]] = choosing
+            self.draw_one(index[0], index[1])
+            self.money[self.order - 1] -= 10
+
+        if self.order == 4 and 35 <= int(choosing) <= 37:
+            if self.money[self.order - 1] >= PRIC[int(choosing)]:
+                self.map[index[0]][index[1]] = choosing
+                self.draw_one(index[0], index[1])
+                self.money[self.order - 1] -= PRIC[int(choosing)]
+
+
+        if 27 <= int(choosing) <= 28 and 23 <= int(self.map[index[0]][index[1]]) <= 24:
+            if self.money[self.order - 1] >= PRIC[int(choosing)]:
+                if self.map[index[0]][index[1]] == 23:
+                    choosing = '027'
+                else:
+                    choosing = '028'
+                self.map[index[0]][index[1]] = choosing
+                self.draw_one(index[0], index[1])
+                self.money[self.order - 1] -= PRIC[int(choosing)]
+
+        self.get_profit()
+
 
     def move(self):
+        self.order %= 4
         self.order += 1
+
+        for i in range(4):
+            self.money[i] += self.profits[i]
+
 
 
 class BoardMenu:
@@ -444,17 +640,17 @@ class BoardMenu:
         self.txt10 = Text(MAPS[4], 110, 470, 55, (255, 255, 255))
 
     def render(self, screen):
-        fon = pygame.transform.scale(load_image('fon4.jpg'), (self.w, self.h))
+        fon = pygame.transform.scale(load_image('fon.jpg'), (self.w, self.h))
         screen.blit(fon, (0, 0))
 
 
 class Button:
     def __init__(self, name, x, y):
         self.sprite = pygame.sprite.Sprite()
-        self.imagen = load_image(f'{name}.png')
+        self.imagen = pygame.image.load(f'Buttons/{name}.png')
         self.imagen = pygame.transform.scale(self.imagen, (60, 60))
 
-        self.image = load_image(f'{name}2.png')
+        self.image = pygame.image.load(f'Buttons/{name}2.png')
         self.image = pygame.transform.scale(self.image, (60, 60))
 
         self.sprite.image = self.imagen
@@ -502,11 +698,15 @@ def start_game(map, key):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     board.maptxt()
+                    board.save_game()
                     return
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if dotcheck(event.pos[0], event.pos[1], board.sprite11.rect.x, board.sprite11.rect.y,
                             board.sprite11.rect.width, board.sprite11.rect.height):
-                    start_tiles()
+                    start_tiles(board.money[board.order - 1], board.profits[board.order - 1])
+                elif dotcheck(event.pos[0], event.pos[1], board.sprite12.rect.x, board.sprite12.rect.y,
+                            board.sprite12.rect.width, board.sprite12.rect.height):
+                    board.move()
                 else:
                     board.change_tile(hexfind(event.pos, board.centers))
             elif event.type == pygame.MOUSEMOTION:
@@ -520,16 +720,32 @@ def start_game(map, key):
                             board.sprite11.rect.width, board.sprite11.rect.height):
                     board.sprite11.image = board.but11.get_pressed()
 
+
+                if not dotcheck(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1],
+                            board.sprite12.rect.x, board.sprite12.rect.y,
+                            board.sprite12.rect.width, board.sprite12.rect.height):
+                    board.sprite12.image = board.but12.get_idle()
+
+                if dotcheck(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1],
+                            board.sprite12.rect.x, board.sprite12.rect.y,
+                            board.sprite12.rect.width, board.sprite12.rect.height):
+                    board.sprite12.image = board.but12.get_pressed()
+
+
+        for i in board.knights:
+            for j in i:
+                if j != None:
+                    j.update(board.limit)
         screen.fill((255, 255, 255))
         all_sprites_game.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
 
 
-def start_tiles():
+def start_tiles(money, profit):
     global choosing
 
-    tiles = TilesChoose()
+    tiles = TilesChoose(money, profit)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -542,8 +758,14 @@ def start_tiles():
                     if tile_list[i].rect.collidepoint(event.pos):
                         choosing = str(i + 1).rjust(3, '0')
                         return
+                for i in range(4):
+                    if knight_list[i].rect.collidepoint(event.pos):
+                        choosing = f'{(i + 1) * 100}'
+                        return
 
         screen.fill((255, 255, 255))
+        tiles.text.render(screen)
+        tiles.text2.render(screen)
         tiles_choose.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
@@ -562,7 +784,7 @@ def start_redactor(map, key):
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if dotcheck(event.pos[0], event.pos[1], redactor.sprite11.rect.x, redactor.sprite11.rect.y,
                             redactor.sprite11.rect.width, redactor.sprite11.rect.height):
-                    start_tiles()
+                    start_tiles('INFINITe', 'INFINITe')
                 else:
                     redactor.change_tile(hexfind(event.pos, redactor.centers))
             elif event.type == pygame.MOUSEMOTION:
@@ -894,7 +1116,7 @@ txt4 = Text('QUIT', 265, 440, 55, (255, 255, 255))
 but_sprites.add(sprite1, sprite2, sprite3, sprite4)
 
 running = True
-fon = pygame.transform.scale(load_image('fon4.jpg'), (WIDTH, HEIGHT))
+fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
 
 while running:
     for event in pygame.event.get():
